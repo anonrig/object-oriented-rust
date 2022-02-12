c25c5c588a48ff796135151f68b7b71666149aa8
Yagiz Nizipli <yagiz@nizipli.com>
2022-02-12 14:31:02 -0500

    style: properly format all files

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 use crate::vehicles::{Car, Truck, VehicleTraits};
 
-mod road_items;
 mod environment;
+mod road_items;
 mod traits;
 mod vehicles;
 
 fn main() {
-  let mut car: Car = Car {
-    current_speed: 0.0,
-    desired_speed: 65.0,
-  };
+    let mut car: Car = Car {
+        current_speed: 0.0,
+        desired_speed: 65.0,
+    };
 
-  let mut truck1: Truck = Truck {
-    current_speed: 0.0,
-    desired_speed: 55.0,
-    load_weight: 4,
-  };
+    let mut truck1: Truck = Truck {
+        current_speed: 0.0,
+        desired_speed: 55.0,
+        load_weight: 4,
+    };
 
-  let mut truck2: Truck = Truck {
-    current_speed: 0.0,
-    desired_speed: 55.0,
-    load_weight: 8,
-  };
+    let mut truck2: Truck = Truck {
+        current_speed: 0.0,
+        desired_speed: 55.0,
+        load_weight: 8,
+    };
 
-  let mut vehicles: Vec<&mut dyn VehicleTraits> = Vec::new();
+    let mut vehicles: Vec<&mut dyn VehicleTraits> = Vec::new();
 
-  vehicles.push(&mut car);
-  vehicles.push(&mut truck1);
-  vehicles.push(&mut truck2);
+    vehicles.push(&mut car);
+    vehicles.push(&mut truck1);
+    vehicles.push(&mut truck2);
 
-  for _n in 1..11 {
-    for vehicle in vehicles.iter_mut()
-    {
-      vehicle.update_speed(1.0);
-      println!("{} speed: {} mph", vehicle.get_type(), vehicle.get_current_speed());
+    for _n in 1..11 {
+        for vehicle in vehicles.iter_mut() {
+            vehicle.update_speed(1.0);
+            println!(
+                "{} speed: {} mph",
+                vehicle.get_type(),
+                vehicle.get_current_speed()
+            );
+        }
     }
-  }
 }
--- src/vehicles.rs
+++ src/vehicles.rs
@@ -1,5 +1,5 @@
-use derivative::Derivative;
 use crate::traits::DynamicRoadItemTraits;
+use derivative::Derivative;
 
 static ACC_RATE: f32 = 3.5;
 static ACC_RATE_EMPTY: f32 = 2.5;
@@ -10,85 +10,108 @@ static DEC_RATE_FULL: f32 = 2.0;
 static MPS_TO_MPH: f32 = 2.237;
 
 pub trait VehicleTraits {
+    fn get_type(&self) -> &str;
+    fn get_current_speed(&self) -> f32;
+    fn get_desired_speed(&self) -> f32;
+    fn accelerate(&mut self, seconds_delta: f32);
+    fn decelerate(&mut self, seconds_delta: f32);
+    fn set_current_speed(&mut self, speed: f32);
 
-  fn get_type(&self) -> &str;
-  fn get_current_speed(&self) -> f32;
-  fn get_desired_speed(&self) -> f32;
-  fn accelerate(&mut self, seconds_delta: f32);
-  fn decelerate(&mut self, seconds_delta: f32);
-  fn set_current_speed(&mut self, speed: f32);
-
-  fn update_speed(&mut self, seconds: f32) {
-    if self.get_current_speed() > self.get_desired_speed() {
-      self.decelerate(seconds);
-    } else {
-      self.accelerate(seconds);
+    fn update_speed(&mut self, seconds: f32) {
+        if self.get_current_speed() > self.get_desired_speed() {
+            self.decelerate(seconds);
+        } else {
+            self.accelerate(seconds);
+        }
     }
-  }
 }
 
 #[derive(Derivative)]
 #[derivative(Debug, Default)]
 pub struct Car {
-  #[derivative(Default(value = "0.0"))]
-  pub current_speed: f32,
-  #[derivative(Default(value = "0.0"))]
-  pub desired_speed: f32,
+    #[derivative(Default(value = "0.0"))]
+    pub current_speed: f32,
+    #[derivative(Default(value = "0.0"))]
+    pub desired_speed: f32,
 }
 
 impl DynamicRoadItemTraits for Car {}
 
 impl VehicleTraits for Car {
-  fn get_type(&self) -> &str { "Car" }
-  fn get_current_speed(&self) -> f32 { self.current_speed }
-  fn get_desired_speed(&self) -> f32 { self.desired_speed }
+    fn get_type(&self) -> &str {
+        "Car"
+    }
+    fn get_current_speed(&self) -> f32 {
+        self.current_speed
+    }
+    fn get_desired_speed(&self) -> f32 {
+        self.desired_speed
+    }
 
-  fn accelerate(&mut self, seconds_delta: f32) {
-    self.current_speed += ACC_RATE * seconds_delta * MPS_TO_MPH;
-  }
+    fn accelerate(&mut self, seconds_delta: f32) {
+        self.current_speed += ACC_RATE * seconds_delta * MPS_TO_MPH;
+    }
 
-  fn decelerate(&mut self, seconds_delta: f32) {
-    self.current_speed -= DEC_RATE * seconds_delta * MPS_TO_MPH;
-  }
+    fn decelerate(&mut self, seconds_delta: f32) {
+        self.current_speed -= DEC_RATE * seconds_delta * MPS_TO_MPH;
+    }
 
-  fn set_current_speed(&mut self, speed: f32) {
-    self.current_speed = if self.current_speed <= speed
-      { speed.min(self.desired_speed) }
-      else { speed.max(self.desired_speed) }
-  }
+    fn set_current_speed(&mut self, speed: f32) {
+        self.current_speed = if self.current_speed <= speed {
+            speed.min(self.desired_speed)
+        } else {
+            speed.max(self.desired_speed)
+        }
+    }
 }
 
 #[derive(Derivative)]
 #[derivative(Debug, Default)]
 pub struct Truck {
-  #[derivative(Default(value = "0.0"))]
-  pub current_speed: f32,
-  #[derivative(Default(value = "0.0"))]
-  pub desired_speed: f32,
-  #[derivative(Default(value = "0"))]
-  pub load_weight: u32,
+    #[derivative(Default(value = "0.0"))]
+    pub current_speed: f32,
+    #[derivative(Default(value = "0.0"))]
+    pub desired_speed: f32,
+    #[derivative(Default(value = "0"))]
+    pub load_weight: u32,
 }
 
 impl DynamicRoadItemTraits for Truck {}
 
 impl VehicleTraits for Truck {
-  fn get_type(&self) -> &str { "Truck" }
-  fn get_current_speed(&self) -> f32 { self.current_speed }
-  fn get_desired_speed(&self) -> f32 { self.desired_speed }
+    fn get_type(&self) -> &str {
+        "Truck"
+    }
+    fn get_current_speed(&self) -> f32 {
+        self.current_speed
+    }
+    fn get_desired_speed(&self) -> f32 {
+        self.desired_speed
+    }
 
-  fn accelerate(&mut self, seconds_delta: f32) {
-    let constant = if self.load_weight <= 5 { ACC_RATE_EMPTY } else { ACC_RATE_FULL };
-    self.current_speed += constant * seconds_delta * MPS_TO_MPH;
-  }
+    fn accelerate(&mut self, seconds_delta: f32) {
+        let constant = if self.load_weight <= 5 {
+            ACC_RATE_EMPTY
+        } else {
+            ACC_RATE_FULL
+        };
+        self.current_speed += constant * seconds_delta * MPS_TO_MPH;
+    }
 
-  fn decelerate(&mut self, seconds_delta: f32) {
-    let constant = if self.load_weight <= 5 { DEC_RATE_EMPTY } else { DEC_RATE_FULL };
-    self.current_speed -= constant * seconds_delta * MPS_TO_MPH;
-  }
+    fn decelerate(&mut self, seconds_delta: f32) {
+        let constant = if self.load_weight <= 5 {
+            DEC_RATE_EMPTY
+        } else {
+            DEC_RATE_FULL
+        };
+        self.current_speed -= constant * seconds_delta * MPS_TO_MPH;
+    }
 
-  fn set_current_speed(&mut self, speed: f32) {
-    self.current_speed = if self.current_speed <= speed
-      { speed.min(self.desired_speed) }
-      else { speed.max(self.desired_speed) }
-  }
+    fn set_current_speed(&mut self, speed: f32) {
+        self.current_speed = if self.current_speed <= speed {
+            speed.min(self.desired_speed)
+        } else {
+            speed.max(self.desired_speed)
+        }
+    }
 }
