7f679b9f72a1b17ee7b690c2ccb6fcaeb073a955
Yagiz Nizipli <yagiz@nizipli.com>
2022-02-20 16:58:24 -0500

    refactor: simplify gui class

--- src/gui.rs
+++ src/gui.rs
@@ -8,37 +8,18 @@ pub trait GUITraits {
     fn get_speed_limit(&self) -> f32;
 }
 
-pub struct MetricGUI {
+pub struct GUI {
     pub output: Box<dyn SimOutput>,
     pub speed_limit: f32,
 }
 
-impl GUITraits for MetricGUI {
+impl GUITraits for GUI {
     fn get_speed(&self, vehicle: &dyn VehicleTraits) -> f32 {
         self.output.get_speed(vehicle) * MPS_TO_KPH
     }
 
     fn get_speed_unit(&self) -> String {
-        "kph".to_string()
-    }
-
-    fn get_speed_limit(&self) -> f32 {
-        self.speed_limit / MPS_TO_KPH
-    }
-}
-
-pub struct ImperialGUI {
-    pub output: Box<dyn SimOutput>,
-    pub speed_limit: f32,
-}
-
-impl GUITraits for ImperialGUI {
-    fn get_speed(&self, vehicle: &dyn VehicleTraits) -> f32 {
-        self.output.get_speed(vehicle) * MPS_TO_KPH
-    }
-
-    fn get_speed_unit(&self) -> String {
-        "mph".to_string()
+        self.output.get_speed_unit()
     }
 
     fn get_speed_limit(&self) -> f32 {
--- src/output.rs
+++ src/output.rs
@@ -1,4 +1,4 @@
-use crate::gui::{GUITraits, ImperialGUI, MetricGUI};
+use crate::gui::{GUITraits, GUI};
 use crate::OutputType::Metric;
 use crate::VehicleTraits;
 
@@ -10,22 +10,22 @@ pub enum OutputType {
 
 impl OutputType {
     pub fn get_gui(&self, speed_limit: f32) -> Box<dyn GUITraits> {
-        if self == &Metric {
-            Box::new(MetricGUI {
-                speed_limit,
-                output: Box::new(MetricOutput {}),
-            })
+        let output: Box<dyn SimOutput> = if self == &Metric {
+            Box::new(MetricOutput {})
         } else {
-            Box::new(ImperialGUI {
-                speed_limit,
-                output: Box::new(MetricOutput {}),
-            })
-        }
+            Box::new(ImperialOutput {})
+        };
+
+        Box::new(GUI {
+            speed_limit,
+            output,
+        })
     }
 }
 
 pub trait SimOutput {
     fn get_speed(&self, vehicle: &dyn VehicleTraits) -> f32;
+    fn get_speed_unit(&self) -> String;
 }
 
 pub struct ImperialOutput {}
@@ -35,10 +35,16 @@ impl SimOutput for ImperialOutput {
     fn get_speed(&self, vehicle: &dyn VehicleTraits) -> f32 {
         vehicle.get_current_speed()
     }
+    fn get_speed_unit(&self) -> String {
+        "mph".to_string()
+    }
 }
 
 impl SimOutput for MetricOutput {
     fn get_speed(&self, vehicle: &dyn VehicleTraits) -> f32 {
         vehicle.get_current_speed()
+    }
+    fn get_speed_unit(&self) -> String {
+        "kph".to_string()
     }
 }
